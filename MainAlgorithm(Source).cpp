50048ed04a883b53a60cb4e1f4d5dae339069bfc
Vedhas Walke <vedhasywalke@gmail.com>
2020-11-24 16:46:51 -0500

    basic C++ code to start off algorithm prototype

---
+++ MainAlgorithm(Source).cpp
@@ -0,0 +1,16 @@
+// ConsoleApp1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string n;
+    ofstream input1;
+    input1.open("Input1");
+    
+    input1 >> n;
+}
+
