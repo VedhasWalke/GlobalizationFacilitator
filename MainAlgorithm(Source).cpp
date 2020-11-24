b6ebedf00d17d5dab77b2a3bc04a533088e756a9
Vedhas Walke <vedhasywalke@gmail.com>
2020-11-24 16:49:18 -0500

    Basic starting C++ code for algorithm

--- MainAlgorithm(Source).cpp
+++ MainAlgorithm(Source).cpp
@@ -1,5 +1,14 @@
-// ConsoleApp1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+Main Algorithm Prototype
+
+Date Updated: 11/24/20
+Description:
+- Naive Bayes Framework
+
+Features:
+
+
+*/
 
 #include <iostream>
 #include <fstream>
@@ -7,10 +16,8 @@ using namespace std;
 
 int main()
 {
-    string n;
-    ofstream input1;
-    input1.open("Input1");
-    
-    input1 >> n;
+   
+
+
 }
 
