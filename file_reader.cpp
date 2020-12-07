7887eaf075ccb2f9f5665984fb3f354b2b642d94
LiuJ69 <68925246+LiuJ69@users.noreply.github.com>
2020-12-07 18:30:34 -0500

    Add files via upload

--- file_reader.cpp
+++ file_reader.cpp
@@ -3,38 +3,50 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <string>
 
-/*
-Just what we have rn, I'll applicable version later
-*/
-void print(std::vector<std::string> const& input)
-{
-	for (int i = 0; i < input.size(); i++) {
-		std::cout << input.at(i) << ' ';
+using namespace std;
+class ProcessData {
+public:
+	string ReadData (string filename);
+	vector <string> SplitByDel (string data, string delimiter);
+};
+string ProcessData::ReadData(string filename) {
+	string data = "";
+	string lineData;
+	ifstream ReadFile(filename);
+	if (!ReadFile) {
+		cout << "read error";
 	}
+	while (getline(ReadFile, lineData)) {
+		data += lineData + "\n";
+	}
+	return data;
 }
+
+vector <string> ProcessData::SplitByDel(string data, string delimiter) {
+	vector <string> vector_data;
+	int pos = 0;
+	string token;
+	while ((pos = data.find(delimiter)) != string::npos) {
+		token = data.substr(0, pos);
+		vector_data.push_back(token);
+		data.erase(0, pos + delimiter.length());
+	}
+	vector_data.push_back(data);
+	
+	return vector_data;
+}
+
+
 int main() {
-	using namespace std;
-	string array[10];
-	ifstream inFile("sample_text.txt");
-	string line;
-	int loop = 0;
-	if (inFile.is_open())
-	{ 
-		while (!inFile.eof())
-		{
-			getline(inFile, line);
-			array[loop] = line;
-			cout << array[loop] << endl;
-			loop++;
-		}
-		inFile.close();
-	}
-	else
-	{
-		cout << "Can't open file";
-
-	}
-	return 0;
-		
+	
+	ProcessData process;
+	string data = process.ReadData("sample_text.txt");
+	vector <string> vector_data = process.SplitByDel(data, "///");
+	for (auto i = vector_data.begin(); i != vector_data.end(); ++i)
+		std::cout << *i << ' ';
+	
+	
 }
+
